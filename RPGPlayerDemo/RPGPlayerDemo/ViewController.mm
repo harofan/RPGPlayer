81bfc3e359bf78b03d9e7a5ce6e53e07e9edda50
RPGLiker <fanyang_32012@outlook.com>
2018-01-10 17:41:38 +0800

    mp3编码类完成

--- RPGPlayerDemo/RPGPlayerDemo/ViewController.mm
+++ RPGPlayerDemo/RPGPlayerDemo/ViewController.mm
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Mp3Encoder.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    Mp3Encoder *encoder = new Mp3Encoder();
+//    encoder->encode();
+//    delete encoder;
+    
 }
 
 
